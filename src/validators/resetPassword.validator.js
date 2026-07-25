cf96e6518125a7caa8fecf24b9e511be18e7c2d3
Nnoli Higgs <arinzeprogress01@gmail.com>
2026-07-25 20:17:28 +0100

    bug fix

--- src/validators/resetPassword.validator.js
+++ src/validators/resetPassword.validator.js
@@ -5,22 +5,18 @@ import { passwordRegex } from "../constants/regex.js";
 
 const resetPasswordSchema = Joi.object({
 
+    email: Joi.string()
+        .email()
+        .required(),
+
     newPassword: Joi.string()
-
         .pattern(passwordRegex)
-
         .required()
-
         .min(8)
-
         .messages({
-
             "string.pattern.base":
-
                 "Password must contain at least one uppercase letter, one lowercase letter, one number and be at least 8 characters long.",
-
         }),
-
 
     confirmPassword: Joi.any()
         .valid(Joi.ref("newPassword"))
@@ -30,5 +26,6 @@ const resetPasswordSchema = Joi.object({
         }),
 
 });
+
 
 export default resetPasswordSchema;
