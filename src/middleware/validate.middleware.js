9877975fe06d53a423fdf6e88e3a650e8ffc3de8
Nnoli Higgs <arinzeprogress01@gmail.com>
2026-08-01 10:10:50 +0100

    modify validate error handler for better toast

--- src/middleware/validate.middleware.js
+++ src/middleware/validate.middleware.js
@@ -9,7 +9,9 @@ export const validate = (schema) => {
         if (error) {
             return res.status(400).json({
                 success: false,
-                message: "Validation failed.",
+                message: error.details
+                    .map((err) => err.message)
+                    .join("\n"),
                 errors: error.details.map((err) => ({
                     field: err.path.join("."),
                     message: err.message,
