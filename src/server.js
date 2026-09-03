9354af4e31c6e62f7ba906a7e70a6aba52fc2032
Higgs <arinzeprogress01@gmail.com>
2026-09-03 19:50:46 +0100

    more on redis installation and changes

--- src/server.js
+++ src/server.js
@@ -18,9 +18,7 @@ import
     {initializeSocket}
     from "./sockets/socket.server.js";
 
-import {
-    setSocketIO
-} from "./sockets/socket.events.js";
+import redisClient from "./config/redis.config.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -29,13 +27,22 @@ const startServer = async () => {
 
     await connectDB();
 
+    await redisClient.connect();
+
+    await redisClient.set("farmconnect:test", "Redis is working!");
+
+    const testValue = await redisClient.get("farmconnect:test");
+
+    console.log("Redis test value:", testValue);
+
+    await redisClient.del("farmconnect:test");
+
     startListingExpirationJob();
     startReservationExpirationJob();
 
     const server = http.createServer(app);
 
-    const io = initializeSocket(server);
-    setSocketIO(io);
+    initializeSocket(server);
 
     server.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
